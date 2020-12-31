2110e75ba4e2063a56b3d367ea2160fcda5fa246
Brandon G <44515406+bgriffis8989@users.noreply.github.com>
2020-12-31 13:35:30 -0500

    Add files via upload

---
+++ Brandon Griffis/js/javascript.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(bglogo).fadeIn(2000);
+    $(".profilepic").fadeIn(2000);  
+
+    //$(document).on('click', 'ul li', function(){
+     // $(this).addClass('active')
+  //  });
+
+  });
+
+  
